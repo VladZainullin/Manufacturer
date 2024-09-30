319226a09b7002ee0bc998e54c5b9a66fba36e45
Vladislav Zainullin <mittoface02@mail.ru>
2024-09-30 23:27:58 +0300

    Изменяет неправильный роут

--- Web/Controllers/ManufacturersController.cs
+++ Web/Controllers/ManufacturersController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers;
 
-[Route("manufacturers")]
+[Route("api/manufacturers")]
 public sealed class ManufacturersController : AppController
 {
     [HttpGet]
